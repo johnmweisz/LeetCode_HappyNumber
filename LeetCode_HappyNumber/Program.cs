ba6532f2e161f373928a0fb6a83949e4c89988ec
John Weisz <johnmweisz@gmail.com>
2019-12-18 18:37:45 -0800

    Add project files.

---
+++ LeetCode_HappyNumber/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_HappyNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var solution = new Solution();
+            Console.WriteLine(solution.IsHappy(19));
+        }
+    }
+
+    public class Solution
+    {
+        public bool IsHappy(int n)
+        {
+            var hash = new HashSet<int>();
+            while (true)
+            {
+                var newTotal = 0;
+                while (n > 0)
+                {
+                    var digit = n % 10;
+                    newTotal += digit * digit;
+                    n /= 10;
+                }
+
+                n = newTotal;
+                if (!hash.Add(n)) return false;
+                if (n == 1) return true;
+            }
+        }
+    }
+}
